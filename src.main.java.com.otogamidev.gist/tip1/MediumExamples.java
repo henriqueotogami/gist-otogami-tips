d9e8c13fe82971156bedb416e678f6c4e662e4a9
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2023-04-20 00:35:30 -0300

    Adicionando o primeiro exemplo do artigo do Medium

---
+++ src.main.java.com.otogamidev.gist/tip1/MediumExamples.java
@@ -0,0 +1,9 @@
+package tip1;
+
+public class MediumExamples {
+    public static void main(String[] args) {
+        final byte byteDataType = ((byte) 207);
+        System.out.println("MediumExamples - byteDataType = " + byteDataType);
+//        MediumExamples - byteDataType = -49
+    }
+}
