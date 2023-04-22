4e55ae2465cedf39d5d4a1bf5325587728dd8619
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2023-04-21 23:13:25 -0300

    Criando classe de exemplo do Complemento de Dois

--- src.main.java.com.otogamidev.gist/tip1/MediumExamples.java
+++ src.main.java.com.otogamidev.gist/tip1/MediumExamples.java
@@ -1,9 +1,15 @@
 package tip1;
 
 public class MediumExamples {
+    final static int decimalToParser = 207;
     public static void main(String[] args) {
-        final byte byteDataType = ((byte) 207);
+
+        final byte byteDataType = ((byte) decimalToParser);
         System.out.println("MediumExamples - byteDataType = " + byteDataType);
 //        MediumExamples - byteDataType = -49
+
+        final int twosComplement = TwosComplement.calculate(decimalToParser);
+        System.out.println("MediumExamples - twosComplement = " + twosComplement);
+//        MediumExamples - twosComplement = -49
     }
 }
