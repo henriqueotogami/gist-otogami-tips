1f0d2c7e775337282f1add9e43c8c947238f8742
Henrique Matheus Alves Pereira <henrique.map@outlook.com>
2023-05-22 21:06:43 -0300

    Ajustes e adição de exemplos do artigo do Medium

--- src/main/java/com/otogamidev/gist/tip1/MediumExamples.java
+++ src/main/java/com/otogamidev/gist/tip1/MediumExamples.java
@@ -1,19 +1,23 @@
 package tip1;
 
 public class MediumExamples {
-    final static int decimalToParser = 10;
+    static int decimalToParser = 10;
     public static void main(String[] args) {
 
-        final byte byteDataType = ((byte) decimalToParser);
-        System.out.println("MediumExamples - byteDataType = " + byteDataType);
+        for (int index = 0; 300 > index; index++) {
+            decimalToParser = index;
+            System.out.println("MediumExamples - decimalToParser = " + decimalToParser);
+            final byte byteDataType = ((byte) decimalToParser);
+            System.out.println("MediumExamples - byteDataType = " + byteDataType);
 //        MediumExamples - byteDataType = -49
 
-        final int twosComplement = TwosComplement.calculate(decimalToParser);
-        System.out.println("MediumExamples - twosComplement = " + twosComplement);
+            final int twosComplement = TwosComplement.calculate(decimalToParser);
+            System.out.println("MediumExamples - twosComplement = " + twosComplement);
 //        MediumExamples - twosComplement = -49
 
-        final int twosComplementOnJava = TwosComplementOnJava.calculate(decimalToParser);
-        System.out.println("MediumExamples - twosComplementOnJava = " + twosComplementOnJava);
+            final int twosComplementOnJava = TwosComplementOnJava.calculate(decimalToParser);
+            System.out.println("MediumExamples - twosComplementOnJava = " + twosComplementOnJava);
 //        MediumExamples - twosComplementOnJava = -49
+        }
     }
 }
--- src/main/java/com/otogamidev/gist/tip1/TwosComplementOnJava.java
+++ src/main/java/com/otogamidev/gist/tip1/TwosComplementOnJava.java
@@ -4,19 +4,19 @@ public class TwosComplementOnJava {
     static int calculate(final int decimalToParser) {
         final int BYTE_MIN_VALUE = -127;
         final int BYTE_MAX_VALUE = 127;
-        System.out.println("-------------------------------------------------------------------------");
-        System.out.println("twoComplementOnJava - calculate(): BEGIN ");
+//        System.out.println("-------------------------------------------------------------------------");
+//        System.out.println("twoComplementOnJava - calculate(): BEGIN ");
         int integerFirstComplement = 0;
 
         if((BYTE_MIN_VALUE > decimalToParser) || (decimalToParser > BYTE_MAX_VALUE)) {
-            System.out.println("twoComplementOnJava - calculate(): starting");
+//            System.out.println("twoComplementOnJava - calculate(): starting");
             integerFirstComplement = TwosComplement.calculate(decimalToParser);
         } else {
-            System.out.println("twoComplementOnJava - calculate(): continues");
+//            System.out.println("twoComplementOnJava - calculate(): continues");
             integerFirstComplement = decimalToParser;
         }
-        System.out.println("ComparingPrimitiveTypes - twoComplementOnJava(): END ");
-        System.out.println("-------------------------------------------------------------------------");
+//        System.out.println("ComparingPrimitiveTypes - twoComplementOnJava(): END ");
+//        System.out.println("-------------------------------------------------------------------------");
 
         return integerFirstComplement;
     }
